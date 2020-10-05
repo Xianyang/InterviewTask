f3be5dab487b2e906ba6d8e546d3b4645d9ebecc
xianyang <>
2020-10-05 23:42:37 +0800

    update to Cupertino style

--- flutter_qr_for_string/lib/main.dart
+++ flutter_qr_for_string/lib/main.dart
@@ -1,20 +1,95 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:qr_flutter/qr_flutter.dart';
 
+final GlobalKey<NavigatorState> firstTabNavKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> secondTabNavKey = GlobalKey<NavigatorState>();
+final GlobalKey<NavigatorState> thirdTabNavKey = GlobalKey<NavigatorState>();
+
 void main() {
-  runApp(InterviewTask());
+  runApp(
+    new CupertinoApp(
+      home: new HomeScreen(),
+      localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
+    ),
+  );
 }
 
-class InterviewTask extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code),
+            title: Text('QR Code'),
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        if (index == 0) {
+          return CupertinoTabView(
+            navigatorKey: firstTabNavKey,
+            builder: (BuildContext context) => FirstTab(),
+          );
+        } else if (index == 1) {
+          return CupertinoTabView(
+            navigatorKey: secondTabNavKey,
+            builder: (BuildContext context) => SecondTab(),
+          );
+        }
+      },
+    );
+  }
+}
+
+class FirstTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyTopBar(
+      text: "Home",
+      uniqueHeroTag: 'tab1',
+      child: Scaffold (
+        body: Center(
+          child: Text('Purposely left empty')
+        )
+      ));
+  }
+}
+
+class SecondTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyTopBar(
+      text: "QR Code",
+      uniqueHeroTag: 'tab1',
+      child: QRCodeView(),
+    );
+  }
+}
+
+class QRCodeView extends StatefulWidget {
   @override
 
-  _InterviewTaskState createState() => _InterviewTaskState();
+  _QRCodeViewState createState() => _QRCodeViewState();
 
 }
 
-class _InterviewTaskState extends State<InterviewTask> {
+class _QRCodeViewState extends State<QRCodeView> {
   var qrContent = '';
 
   @override
@@ -26,59 +101,34 @@ class _InterviewTaskState extends State<InterviewTask> {
   }
 
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.qr_code)),
-              ],
-            ),
-            title: Text('Interview Task'),
-          ),
-          body: TabBarView(
-            children: [
-              Scaffold (
-                body: Center(
-                  child: Text('Purposely left empty')
-                )
-              ),
-              Scaffold (
-                body: Center(child: Column(children: <Widget>[  
-                  Container(  
-                    margin: EdgeInsets.all(10),  
-                    child: QrImage(
-                      data: qrContent,
-                      version: QrVersions.auto,
-                      size: 300,
-                      gapless: false,
-                    )
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text("String in the QR code: " + qrContent),
-                  ),
-                  Container(  
-                    margin: EdgeInsets.all(10),  
-                    child: FlatButton(  
-                      color: Colors.blue,
-                      highlightColor: Colors.blue[700],
-                      colorBrightness: Brightness.dark,
-                      splashColor: Colors.grey,
-                      child: Text("Generate a new QR code"),
-                      shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-                      onPressed: _generateNewQRCode,  
-                    ),  
-                  ),
-                ])) 
-              ),
-            ],
-          ),
+    return Scaffold (
+      body: Center(child: Column(children: <Widget>[  
+        Container(  
+          margin: EdgeInsets.only(top: 100),
+          child: QrImage(
+            data: qrContent,
+            version: QrVersions.auto,
+            size: 300,
+            gapless: false,
+          )
         ),
-      ),
+        Container(
+          margin: EdgeInsets.all(10),
+          child: Text("String in the QR code: " + qrContent),
+        ),
+        Container(  
+          margin: EdgeInsets.all(10),  
+          child: FlatButton(  
+            color: Colors.blue,
+            highlightColor: Colors.blue[700],
+            colorBrightness: Brightness.dark,
+            splashColor: Colors.grey,
+            child: Text("Generate a new QR code"),
+            shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            onPressed: _generateNewQRCode,  
+          ),  
+        ),
+      ])) 
     );
   }
 
@@ -96,5 +146,35 @@ class _InterviewTaskState extends State<InterviewTask> {
     } else {
       print('fail to retrieve random string');
     }
+  }
+}
+
+class MyTopBar extends StatelessWidget {
+  final String text;
+
+  final TextStyle style;
+  final String uniqueHeroTag;
+  final Widget child;
+
+  MyTopBar({
+    this.text,
+    this.style,
+    this.uniqueHeroTag,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          heroTag: uniqueHeroTag,
+          transitionBetweenRoutes: false,
+          middle: Text(
+            text,
+            style: style,
+          ),
+        ),
+        child: child,
+      );
   }
 }
